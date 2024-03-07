106761f1182dc1ef9521e53c19551dc61a7f2852
osatuyi <osatuyipikin@gmail.com>
2024-03-06 16:46:09 -0800

    Finish Assignment

--- contracts/1_Storage.sol
+++ contracts/1_Storage.sol
@@ -7,22 +7,44 @@ pragma solidity >=0.8.2 <0.9.0;
  * @dev Store & retrieve value in a variable
  * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
  */
-contract Storage {
+contract Assignment {
 
-    uint256 number;
+    string name;
+    uint256 sum;
+    uint256 dividedValue;
 
     /**
      * @dev Store value in variable
-     * @param num value to store
+     * @param currName value to store
      */
-    function store(uint256 num) public {
-        number = num;
+    function storeName(string memory currName) public {
+        name = currName;
     }
 
     /**
      * @dev Return value 
      * @return value of 'number'
      */
-    
-    
+    function retrieveName() public view returns (string memory){
+        return name;   
+    }
+
+    /**
+     * @dev Add numbers & return value 
+     * @return value of 'sum'
+     */
+
+    function addNumbers(uint256 num1, uint256 num2) public returns (uint256){
+        sum = num1 + num2;
+        return sum;
+    }
+
+   /**
+     * @dev Divide numbers & return value 
+     * @return value of 'dividedValue'
+     */
+     function divideNumbers(uint256 num1, uint256 num2) public returns (uint256){
+        dividedValue = num1 / num2;
+        return dividedValue;
+        }
 }
